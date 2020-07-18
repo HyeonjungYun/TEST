4bd13ed08f016d31ae61489919e2f4d83e7c4d9c
hjy9971 <yjn5858@naver.com>
2020-07-19 01:24:12 +0900

    if문 연습

---
+++ if문.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+
+    a < 11;
+
+    cout<<"What your favorite number? (0 < your Favorite number =< 10) \n";
+    cin>>a;
+
+    if (a % 2 == 1)
+    cout<<"your satan";
+
+    if (a % 2 == 0)
+    cout<<"your pervert";
+
+    return 0;
+}
