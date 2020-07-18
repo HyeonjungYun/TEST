cd2110b5f06ea3eb6787f563191127f27474a42a
hjy9971 <yjn5858@naver.com>
2020-07-18 20:45:31 +0900

    A-B

--- A-B.cpp
+++ A-B.cpp
@@ -6,15 +6,15 @@ int main()
    int a,b,d;
     a>0;
     b<10001;
+
+    cin>>a>>b;
   
    cout<<fixed;
    cout.precision(10);
 
- 
+   d=a-b; 
 
-   cout<<c<<endl;
-
-   d=a-b;
+   cout<<d<<endl;
 
  
 
