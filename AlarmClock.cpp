123983c6f56937a2f8ec95a24974cec0b06b31cd
hjy9971 <yjn5858@naver.com>
2020-07-19 06:23:13 +0900

    백준 if문 5번째 문제

---
+++ AlarmClock.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e,f;
+    ((a,d>=0) && (a,d<25));
+    ((b,c>=0) && (b,c<61));
+
+    cin>>a>>b;
+
+    c=b-45;
+
+    if(c<0 && a == 0){
+      
+        
+        d=a-1;
+        e=c+60;
+        f=d+24;
+        cout<<f<<" "<<e;
+    }
+
+    else if(c<0){
+        d=a-1;
+        e=c+60;
+        
+        cout<<d<<" "<<e;
+    }
+
+    else
+    {
+        cout<<a<<" "<<c<<endl;
+        
+    }
+    
+
+    
+
+    
+    
+
+    return 0;
+
+
+
+
+}
