60325680922666f7c9563799cc48fdd355bd8800
hjy9971 <yjn5858@naver.com>
2020-07-19 01:15:34 +0900

    부울식

---
+++ bool.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool b2 = 1.2 ;
+    cout<<b2<<"\n" ;
+    bool b4 = 0 ;
+    cout<<b4<<"\n";
+
+    bool light = true;
+    cout<<light;
+    
+}
