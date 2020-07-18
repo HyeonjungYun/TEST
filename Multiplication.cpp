f6b7bdc10199d6e4a88c0373a39fd7f38a063108
hjy9971 <yjn5858@naver.com>
2020-07-19 05:02:13 +0900

    백준 사칙연산 마지막 문제

---
+++ Multiplication.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e,f;
+    a , b>99 && a , b<1000;
+
+    cin>>a>>b;
+
+    c=a*b;
+
+    d=(b%10);
+    b=b/10;
+    e=(b%10);
+    b=b/10;
+    f=(b%10);
+
+    cout<<a*d<<"\n";
+    cout<<a*e<<"\n";
+    cout<<a*f<<"\n";
+    cout<<c<<endl;
+
+
+
+
+    
+
+
+}
