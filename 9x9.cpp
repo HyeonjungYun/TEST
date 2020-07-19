6d82d52e056641087d34449625ebbc9bd7016994
hjy9971 <yjn5858@naver.com>
2020-07-19 16:40:27 +0900

    백준 for문 2번째 문제

---
+++ 9x9.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cin>>c;
+    for(a=1; a<10; a++)
+    {
+        
+        b=c*a;
+        cout<<c<<" * "<<a<<" = "<<b<<endl;
+
+    }
+    return 0;
+}
