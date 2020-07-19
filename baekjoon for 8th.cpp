e13f552814145b442ed121f1ace9d6a3de2743b3
hjy9971 <yjn5858@naver.com>
2020-07-19 18:37:33 +0900

    백준 for문 8번째

---
+++ baekjoon for 8th.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+int main()
+{
+    int a,b;
+    
+    
+    cin>>a; 
+
+    for(b = 1; b <= a; b++){
+        
+        for(int c =1; c <= b; c++){
+        cout<<"*";
+    }
+    cout<<"\n";
+    }
+
+
+   
+   
+   
+
+    return 0;
+}
