86762c9d2fca95dc407ac268983bbbeb8dee3fae
hjy9971 <yjn5858@naver.com>
2020-07-19 21:33:12 +0900

    백준 while문 2번째

---
+++ baekjoon while 2rd.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+
+    int a,b;
+
+    a > 0;
+    b < 10;
+    
+
+    while(cin>>a>>b) {
+
+    
+
+
+
+    cout<<a+b<<"\n";
+}
+
+
+
+
+
+    return 0;
+}
