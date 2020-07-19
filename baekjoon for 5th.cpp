a7be95da87fa627f6b4c988b539f9535e31a4fe9
hjy9971 <yjn5858@naver.com>
2020-07-19 18:02:56 +0900

    백준 for 5번째

---
+++ baekjoon for 5th.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+
+int main()
+{
+    int a,b;
+    
+    cin>>a; 
+
+    for(b = a; b >= 1; b--){
+        cout<<b<<"\n";
+
+    }
+   
+   
+   
+
+    return 0;
+}
