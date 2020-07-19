9a5a0682f1f91866ced8563c46ad4d3ae96ba614
hjy9971 <yjn5858@naver.com>
2020-07-19 18:06:04 +0900

    백준 for문 6번째

---
+++ baekjoon for 6th.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e;
+    cin>>a;
+
+    for(b = 1; b <= a; b++ ){
+        cin>>d>>e;
+        c = d + e;
+        cout<<"Case #"<<b<<": "<<c<<endl;
+    }
+    return 0;
+
+
+}
