29aa421eac0d715882a32e72f04ee35a53a84f80
hjy9971 <yjn5858@naver.com>
2020-07-19 17:28:36 +0900

    백준 for문 3번째

---
+++ baekjun for 3rd.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    
+    cin>>a; 
+
+    int c = 0;
+    for(b = 1; b <= a; b++){
+        c+=b;
+
+    }
+   
+    cout<<c<<endl;
+
+    return 0;
+}
