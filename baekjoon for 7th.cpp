21dd5a599a8f357148e8c71f2a202a612d8dec9f
hjy9971 <yjn5858@naver.com>
2020-07-19 18:10:41 +0900

    백준 for문 7번째

---
+++ baekjoon for 7th.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d,e;
+    cin>>a;
+    
+
+    for(b = 1; b <= a; b++ ){
+        (d,e >0 && d,e <10);
+        cin>>d>>e;
+        c = d + e;
+        cout<<"Case #"<<b<<": "<<d<<" + "<<e<<" = "<<c<<"\n";
+    }
+    return 0;
+
+
+}
